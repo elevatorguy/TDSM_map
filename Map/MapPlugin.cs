c8d59212c5b2cbe258e6c33a51db6264fb279b47
elevatorguy <elevatorguy@gmail.com>
2015-08-24 18:22:54 -0500

    References updated for TShock 4.3.9

--- Map/MapPlugin.cs
+++ Map/MapPlugin.cs
@@ -82,7 +82,7 @@ namespace Map
         }
         public override Version Version
         {
-            get { return new Version(4, 3, 8, 0816); } //Version number reflects tshock version, and date map plugin was updated.
+            get { return new Version(4, 3, 9, 0824); } //Version number reflects tshock version, and date map plugin was updated.
         }
 
         public override void Initialize()
